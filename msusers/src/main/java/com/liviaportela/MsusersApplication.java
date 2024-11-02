40fdbce278725df84f21674502acbd5023ee1f7e
Lívia <liviaportelaferreira@gmail.com>
2024-11-02 00:52:40 -0300

    feature: msusers base

---
+++ msusers/src/main/java/com/liviaportela/MsusersApplication.java
@@ -0,0 +1,15 @@
+package com.liviaportela;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+public class MsusersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsusersApplication.class, args);
+	}
+
+}
